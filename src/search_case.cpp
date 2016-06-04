9e716b540eef001ddae763611e8ca7fb97f6a515
br0ce <beck@informatik.uni-bonn.de>
2016-06-04 12:31:13 +0200

    fixed bug in Search_case::pb_load_maze_clicked()

--- src/search_case.cpp
+++ src/search_case.cpp
@@ -88,7 +88,7 @@ void Search_case::pb_load_maze_clicked()
       int k = 0;
       for(auto i = 0; i < m.rows(); ++i)
         for(auto j = 0; j < m.cols(); ++j)
-          m(i, j) = v.at(k);
+          m(i, j) = v.at(k++);
 
       map_ = m;
     }
